7b0ea52799ddadb22d9ee91fd2707585ac3c0a55
Leonard <86230878+Wrdn28@users.noreply.github.com>
2023-02-27 12:48:48 +0700

    X01AD: Nuke some package

--- device.mk
+++ device.mk
@@ -112,8 +112,8 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@5.0-impl \
-    # android.hardware.audio.effect@2.0-service \
-    android.hardware.soundtrigger@2.2-impl
+    android.hardware.soundtrigger@2.2-impl \
+    # android.hardware.audio.effect@2.0-service 
 
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
